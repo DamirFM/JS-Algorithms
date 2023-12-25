14b228b60b14c8b3e3edd86f3912662dedad2916
DamirFM <yakupovdamir93@gmail.com>
2023-12-25 17:14:09 -0500

    added Factorial

---
+++ Factorial/factorial.js
@@ -0,0 +1,11 @@
+// Write code to create a function that returns the factorial of `num`
+
+var factorial = function(num) {
+    if (num === 0) {
+        return 1;
+      }
+      else {
+        return num * factorial(num - 1);
+      }
+    
+};
